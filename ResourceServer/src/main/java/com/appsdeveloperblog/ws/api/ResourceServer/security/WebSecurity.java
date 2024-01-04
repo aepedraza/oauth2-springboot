6331c63d45244b3cb4beab26b81545b9d8e201e8
Alvaro Esteban Pedraza <aepedraza3117@gmail.com>
2024-01-04 11:50:38 -0300

    7-61: Register JwtAuthenticationConverter with HttpSecurity

--- ResourceServer/src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/security/WebSecurity.java
+++ ResourceServer/src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/security/WebSecurity.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.oauth2.core.authorization.OAuth2AuthorizationManagers.hasScope;
@@ -15,6 +16,9 @@ public class WebSecurity {
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
 
         // Configure HTTP Security
         http.authorizeHttpRequests(auth ->
@@ -27,7 +31,7 @@ public class WebSecurity {
                                 .hasRole("developer") // .hasAnyRole(String...) also available
                                 .anyRequest().authenticated())
                 // Indicates that the app is an OAuth2.0 auth server and expects JWT
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {}));
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)));
 
         return http.build();
     }
