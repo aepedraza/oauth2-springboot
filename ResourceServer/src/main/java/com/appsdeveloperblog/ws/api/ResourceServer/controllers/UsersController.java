d69162c232e911f51479c7fe285278146d1f2827
Alvaro Esteban Pedraza <aepedraza3117@gmail.com>
2024-01-12 14:48:24 -0300

    8-69: Switched to @PreAuthorize annotation with sample expression

--- ResourceServer/src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/controllers/UsersController.java
+++ ResourceServer/src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/controllers/UsersController.java
@@ -1,6 +1,9 @@
 package com.appsdeveloperblog.ws.api.ResourceServer.controllers;
 
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,9 +19,12 @@ public class UsersController {
         return "Working...";
     }
 
-    @Secured("ROLE_developer") // allows the operation only to users with developer role
+    // Allows more complex expressions, is evaluated before the method is executed
+    // can check roles and parameters properties
+    @PreAuthorize("hasRole('developer') or #id == #jwt.subject")
+    //@Secured("ROLE_developer") // allows the operation only to users with developer role
     @DeleteMapping("/{id}")
-    public String deleteUser(@PathVariable String id) {
-        return "Deleted user with id " + id;
+    public String deleteUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
+        return "Deleted user with id " + id + " and JWT subject " + jwt.getSubject();
     }
 }
