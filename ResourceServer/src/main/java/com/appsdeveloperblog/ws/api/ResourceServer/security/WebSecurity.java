840ce814cbc16eded4381c2dd487e67df26fb11d
Alvaro Esteban Pedraza <aepedraza3117@gmail.com>
2024-01-12 12:36:22 -0300

    8-65: Enabled method level security

--- ResourceServer/src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/security/WebSecurity.java
+++ ResourceServer/src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/security/WebSecurity.java
@@ -1,16 +1,14 @@
 package com.appsdeveloperblog.ws.api.ResourceServer.security;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.oauth2.core.authorization.OAuth2AuthorizationManagers.hasScope;
-
-@Configuration
+@EnableMethodSecurity // Already includes @Configuration
 @EnableWebSecurity // May not be necessary, check Spring Documentation
 public class WebSecurity {
 
