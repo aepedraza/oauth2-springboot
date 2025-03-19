6c5fbe84e97d7284ef39a4f312f48d0e329e312b
Alvaro Esteban Pedraza <aepedraza3117@gmail.com>
2025-03-19 15:27:40 -0300

    15-142: Implemented Code Verifier value generation

---
+++ spa-example/src/main/resources/static/index.js
@@ -0,0 +1,27 @@
+function generateState(length) {
+    var stateValue = "";
+    var alphaNumericCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var alphaNumericCharactersLength = alphaNumericCharacters.length;
+    for (var i = 0; i < length; i++) {
+        stateValue += alphaNumericCharacters.charAt(Math.floor(Math.random() * alphaNumericCharactersLength));
+    }
+
+    document.getElementById("stateValue").innerHTML = stateValue;
+}
+
+function generateCodeVerifier() {
+    var returnValue = "";
+    var randomByteArray = new Uint8Array(32);
+    window.crypto.getRandomValues(randomByteArray);
+
+    returnValue = base64urlencode(randomByteArray);
+
+    document.getElementById("codeVerifierValue").innerHTML = returnValue;
+}
+
+function base64urlencode(sourceValue) {
+    var stringValue = String.fromCharCode.apply(null, sourceValue);
+    var base64Encoded = btoa(stringValue);
+    var base64urlEncoded = base64Encoded.replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+    return base64urlEncoded;
+}
